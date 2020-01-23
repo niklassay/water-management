d611e935d00676c03e7f227607958f9133b69661
Martin Wicke <vanwicke@gmail.com>
2020-01-23 12:46:45 +0100

    Update theoryPlots.m

--- Code/theoryPlots.m
+++ Code/theoryPlots.m
@@ -8,5 +8,9 @@ hold on
 bar(x2,f(x2), 1, "w")
 plot(x2,f(x2), "o", "color", "k")
 plot(x1,f(x1), "color", "#0072BD")
-
+ylabel('f(t)');
+xlabel('t');
+set(gca,'FontSize',8)
+set(gcf,'Units','Centimeters','position',[0,0,16,6]);
+saveas(gcf,'quadratureA.eps', 'epsc');
 sum(f(linspace(0,1,101))/101)
